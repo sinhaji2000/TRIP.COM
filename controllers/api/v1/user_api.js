be3f4cd256a41215191834d864f481f4ce4826b5
sinhaji2000 <sinhaoffcial@gmail.com>
2025-01-17 20:02:07 +0530

    signupApi && updateUserapi

--- controllers/api/v1/user_api.js
+++ controllers/api/v1/user_api.js
@@ -23,3 +23,80 @@ exports.postSignIn = async (req, res) => {
     });
   }
 };
+
+exports.postSignUp = async (req, res) => {
+  try {
+    const { name, email, DOB, password } = req.body;
+
+    const isUserExit = await User.findOne({ email: email });
+    console.log(isUserExit);
+
+    if (isUserExit) {
+      return res.status(422).json({
+        message: "User already exists",
+      });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    let avatarPath = "";
+    if (req.file) {
+      avatarPath = `/uploads/users/avatar/${req.file.filename}`;
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      DOB,
+      avatar: avatarPath,
+      password: hashPassword,
+    });
+    return res.status(200).json({
+      message: "User created successfully",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    // Ensure the user is authorized to update the profile
+    if (req.body._id === req.params.id) {
+      const { name, password } = req.body;
+      let avatarPath = "";
+      if (req.file) {
+        avatarPath = `/uploads/users/avatar/${req.file.filename}`;
+      }
+
+      let hashedPassword = undefined;
+      if (password) {
+        const saltRounds = 10;
+        hashedPassword = await bcrypt.hash(password, saltRounds);
+      }
+
+      const updateFields = {
+        name,
+        avatar: avatarPath,
+      };
+
+      if (hashedPassword) {
+        updateFields.password = hashedPassword; // Add the hashed password only if it exists
+      }
+
+      await User.findByIdAndUpdate(req.params.id, updateFields);
+
+      // Redirect to the profile page after a successful update
+      return res.json(200, {
+        message: "User updated  successfully",
+      });
+    } else {
+      return res.status(403).send("Unauthorized action.");
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
--- routes/api/v1/user_apiRoutes.js
+++ routes/api/v1/user_apiRoutes.js
@@ -3,5 +3,6 @@ const Router = express.Router();
 const user_apiController = require("../../../controllers/api/v1/user_api");
 
 Router.post("/create-session", user_apiController.postSignIn);
-
+Router.post("/create-user", user_apiController.postSignUp);
+Router.post("/update_user/:id", user_apiController.updateUser);
 module.exports = Router;
